ce7424a171b7c496380ca137aae3744ac40dde70
unknown <ibrasah16@gmail.com>
2020-02-24 10:46:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoad: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list')
+      .then(res => res.json(1))
+      .then(json => {
+        this.setState({
+            isLoad: true,
+            items: json,
+        })
+      });  
+  }
+
+  render() {
+
+    var { isLoad, items } = this.state;
+
+    if (!isLoad) {
+      return <div>Loading...</div>
+    }
+    else {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ul>
+         {items.map(item => (
+           <li key={item.id}>
+             Title : {item.title}
+           </li>
+         ))}
+       </ul>
     </div>
-  );
+    )}
+  };
+
 }
 
 export default App;
